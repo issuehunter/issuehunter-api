5541a5464f82a9d0b25a8db0cedb7a3791d77bf5
Giovanni Cappellotto <potomak84@gmail.com>
2017-11-29 15:54:44 -0500

    WIP...

---
+++ TODO.md
@@ -0,0 +1,4 @@
+https://stevenwhite.com/building-a-rest-service-with-golang-2/
+https://codegangsta.gitbooks.io/building-web-apps-with-go/content/deployment/index.html
+
+https://derekchiang.com/posts/reusable-and-type-safe-options-for-go-apis/
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
 )
 
 // Logger is a cool type
@@ -36,15 +38,43 @@ func HelloWorld(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 // App is a cool function
 func App() http.Handler {
 	router := httprouter.New()
+	db := NewDB()
 
 	// Add a handler on /hello
 	router.GET("/hello", HelloWorld)
+
+	router.GET("/campaigns", CampaignsIndex(db))
 
 	// Serve static assets
 	router.GET("/", serveIndex)
 	router.ServeFiles("/stylesheets/*filepath", http.Dir("static/stylesheets"))
 
 	return &Logger{router}
+}
+
+func CampaignsIndex(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		var title, author string
+		err := db.QueryRow("SELECT title, author FROM campaigns").Scan(&title, &author)
+		if err != nil && err == sql.ErrNoRows {
+			fmt.Fprintf(rw, "No campaigns found")
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(rw, "The first campaign is '%s' by '%s'", title, author)
+	}
+}
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("postgres", "postgres://gcappellotto@localhost/issuehunter?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
 
 func main() {
